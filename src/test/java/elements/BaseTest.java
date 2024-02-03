28e715736ecc7f89dcc3e4d5d1d9ae0340c76073
Alex_QA <al694000n@gmail.com>
2024-02-03 18:41:14 +0300

    minor change

--- src/test/java/elements/BaseTest.java
+++ src/test/java/elements/BaseTest.java
@@ -3,7 +3,7 @@ package elements;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
