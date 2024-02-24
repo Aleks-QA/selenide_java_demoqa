c3d078a47e683d3e06945577e61ae93b867ddcd1
Alex_QA <al694000n@gmail.com>
2024-02-25 00:28:33 +0300

    added allure and javadoc

--- src/main/java/pom/AlertsPage.java
+++ src/main/java/pom/AlertsPage.java
@@ -1,10 +1,11 @@
 package pom;
 
-import com.codeborne.selenide.ModalOptions;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.ModalOptions.withTimeout;
+
 import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
 
@@ -12,46 +13,111 @@ import static java.time.Duration.ofSeconds;
  * Страница "Alerts".
  */
 public class AlertsPage {
-    SelenideElement buttonFirstAlert = $("#alertButton"); // alertButton
-    SelenideElement buttonTimerAlert = $("#timerAlertButton"); // timerAlertButton
-    SelenideElement buttonConfirmAlert = $("#confirmButton"); // confirmButton
-    SelenideElement buttonPromptAlert = $("#promtButton"); // promtButton
-    SelenideElement messageConfirmAlert = $("#confirmResult"); // confirmResult
-    SelenideElement messagePromptAlert = $("#promptResult"); // promptResult
+    /**
+     * Локатор кнопки вызова первого Alert.
+     */
+    private final SelenideElement buttonFirstAlert = $("#alertButton");
 
+    /**
+     * Локатор кнопки вызова отложенного Alert.
+     */
+    private final SelenideElement buttonTimerAlert = $("#timerAlertButton");
+
+    /**
+     * Локатор кнопки вызова Confirm.
+     */
+    private final SelenideElement buttonConfirmAlert = $("#confirmButton");
+
+    /**
+     * Локатор кнопки вызова Prompt.
+     */
+    private final SelenideElement buttonPromptAlert = $("#promtButton");
+
+    /**
+     * Локатор получателя сообщения после закрытия Confirm.
+     */
+    private final SelenideElement messageConfirmAlert = $("#confirmResult");
+
+    /**
+     * Локатор получателя сообщения после закрытия Prompt.
+     */
+    private final SelenideElement messagePromptAlert = $("#promptResult");
+
+    /**
+     * Нажатие кнопки вызова первого Alert.
+     */
+    @Step("Нажатие кнопки вызова первого Alert")
     public void clickButtonFirstAlert() {
         buttonFirstAlert.shouldBe(visible).click();
     }
 
+    /**
+     * Нажатие кнопки вызова отложенного Alert.
+     */
+    @Step("Нажатие кнопки вызова отложенного Alert")
     public void clickButtonTimerAlert() {
         buttonTimerAlert.shouldBe(visible).click();
     }
 
+    /**
+     * Нажатие кнопки вызова Confirm.
+     */
+    @Step("Нажатие кнопки вызова Confirm")
     public void clickButtonConfirmAlert() {
         buttonConfirmAlert.shouldBe(visible).click();
     }
 
+    /**
+     * Нажатие кнопки вызова Prompt.
+     */
+    @Step("Нажатие кнопки вызова Prompt")
     public void clickButtonPromptAlert() {
         buttonPromptAlert.shouldBe(visible).click();
     }
 
+    /**
+     * Подтвердить Alert.
+     */
+    @Step("Подтвердить Alert")
     public void confirmAlert() {
         confirm(withTimeout(ofSeconds(15)));
     }
 
-    public void inputTextPromptAlert(String name) {
-        prompt("Please enter your name", name);
-    }
-
+    /**
+     * Отклонить Alert.
+     */
+    @Step("Отклонить Alert")
     public void dismissAlert() {
         dismiss(withTimeout(ofSeconds(15)));
     }
 
+    /**
+     * Ввод текста в Prompt.
+     *
+     * @param name имя для ввода.
+     */
+    @Step("Ввод текста в Prompt")
+    public void inputTextPromptAlert(final String name) {
+        prompt("Please enter your name", name);
+    }
+
+    /**
+     * Получение текста после закрытия Confirm.
+     *
+     * @return полученный текст.
+     */
+    @Step("Получение текста после закрытия Confirm")
     public String getMessageConfirmAlert() {
         messageConfirmAlert.shouldBe(visible);
         return messageConfirmAlert.getText();
     }
 
+    /**
+     * Получение текста после закрытия Prompt.
+     *
+     * @return полученный текст.
+     */
+    @Step("Получение текста после закрытия Prompt.")
     public String getMessagePromptAlert() {
         messagePromptAlert.shouldBe(visible);
         return messagePromptAlert.getText();
--- src/test/java/alerts/AlertsTest.java
+++ src/test/java/alerts/AlertsTest.java
@@ -1,49 +1,75 @@
-package Alerts;
+package alerts;
 
+import elements.BaseTest;
+import io.qameta.allure.Epic;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pom.AlertsPage;
-import pom.ButtonsPage;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class AlertsTest {
+public class AlertsTest extends BaseTest {
 
+    /**
+     * Проверка работы Alert
+     */
     @Test
+    @Epic(value = "Alerts")
+    @DisplayName("Проверка работы Alert")
     public void FirstAlertTest() {
         open("https://demoqa.com/alerts");
+
         AlertsPage alertsPage = new AlertsPage();
         alertsPage.clickButtonFirstAlert();
         alertsPage.confirmAlert();
     }
 
+    /**
+     * Проверка работы Alert с задержкой
+     */
     @Test
+    @Epic(value = "Alerts")
+    @DisplayName("Проверка работы Alert с задержкой")
     public void timerAlertTest() {
         open("https://demoqa.com/alerts");
+
         AlertsPage alertsPage = new AlertsPage();
         alertsPage.clickButtonTimerAlert();
         alertsPage.confirmAlert();
     }
 
+    /**
+     * Проверка отклонения Alert
+     */
     @Test
+    @Epic(value = "Alerts")
+    @DisplayName("Проверка отклонения Alert")
     public void confirmAlertTest() {
         open("https://demoqa.com/alerts");
+
         AlertsPage alertsPage = new AlertsPage();
         alertsPage.clickButtonConfirmAlert();
-        alertsPage.confirmAlert();
+        alertsPage.dismissAlert();
         String message = alertsPage.getMessageConfirmAlert();
-        System.out.println(message);
-        Assertions.assertEquals("You selected Ok", message);
+
+        Assertions.assertEquals("You selected Cancel", message);
     }
 
+    /**
+     * Проверка ввода текста в Alert
+     */
     @Test
+    @Epic(value = "Alerts")
+    @DisplayName("Проверка ввода текста в Alert")
     public void promptAlertTest() {
         open("https://demoqa.com/alerts");
+
         AlertsPage alertsPage = new AlertsPage();
         alertsPage.clickButtonPromptAlert();
         alertsPage.inputTextPromptAlert("test777");
         String message = alertsPage.getMessagePromptAlert();
-        System.out.println(message);
+
         Assertions.assertEquals("You entered test777", message);
     }
 }
